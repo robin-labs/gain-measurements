75ce1a77839259cfd7354418ca2d21efa22d736f
Ian Reynolds <idreyn@gmail.com>
2017-07-13 20:10:12 -0400

    initial

---
+++ analysis/main.py
@@ -0,0 +1,109 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+from scikits.audiolab import wavread
+
+from segment import band_powers
+
+FREQS = [15000, 20000, 25000, 30000, 35000, 40000, 45000]
+PULSE_DURATION = 0.05
+SAMPLE_DURATION = 0.02
+
+def normalize_angle(angle):
+	while not -180 <= angle < 180:
+		angle += 360 * (1 if angle < 0 else -1)
+	return angle * (np.pi / 180)
+
+# References is a dict mapping experiment csv => folder
+def get_files(references):
+	readings = []
+	azimuth_files = {}
+	for ref in references:
+		path_template = references[ref]
+		with open(ref,'r') as csvfile:
+			reader = csv.reader(csvfile)
+			reader.next()
+			for row in reader:
+				azimuth = int(row[0])
+				files = [path_template % (e.zfill(4)) for e in row[1:]]
+				if not azimuth_files.get(azimuth):
+					azimuth_files[azimuth] = []
+				azimuth_files[azimuth] += files
+	return azimuth_files
+
+def extract_powers(azimuth_files, sample_duration=SAMPLE_DURATION):
+	azimuth_by_freq = {}
+	for f in FREQS:
+		azimuth_by_freq[f] = {}
+	for a in sorted(azimuth_files):
+		for file in azimuth_files[a]:
+			data, fs, enc = wavread(file)
+			print "Calculating azimuth powers", a
+			powers = band_powers(data, fs, FREQS,
+				PULSE_DURATION, sample_duration, debug=False)
+			for f in powers:
+				if not azimuth_by_freq[f].get(a):
+					azimuth_by_freq[f][a] = []
+				azimuth_by_freq[f][a].append(powers[f])
+	return azimuth_by_freq
+
+def normalize_band(azimuths):
+	assert azimuths[0]
+	for a in azimuths:
+		azimuths[a] = np.mean(azimuths[a])
+	db_azimuths = {}
+	for a in azimuths:
+		db_azimuths[a] = 10 * np.log10(azimuths[a] / azimuths[0])
+	return db_azimuths
+
+def normalize(azimuth_powers):
+	db_azimuths_freqs = {}
+	for freq in azimuth_powers:
+		db_azimuths_freqs[freq] = normalize_band(azimuth_powers[freq])
+	return db_azimuths_freqs
+
+def plot_gain_pattern(azimuth_powers, db_azimuths_freqs, filename):
+	ax = plt.subplot(111, projection='polar')
+	for freq in sorted(db_azimuths_freqs):
+		rel = 10 * np.log10(azimuth_powers[freq][0] / azimuth_powers[FREQS[0]][0])
+		powers_dict = db_azimuths_freqs[freq]
+		azimuths = np.array(map(normalize_angle,sorted(powers_dict.keys())))
+		# Aaaaaaah --- we need azimuths to be monotonic for splice to work
+		slice_index = - (np.argmax(np.diff(azimuths) < 0) + 1)
+		azimuths = np.roll(azimuths, slice_index)
+		powers = np.roll(
+			np.array(
+				[powers_dict[p] + rel for p in sorted(powers_dict.keys())]
+			),
+			slice_index
+		)
+		azimuths_smooth = np.linspace(azimuths.min(), azimuths.max(), 1e3)
+		powers_smooth = spline(azimuths, powers, azimuths_smooth)
+		ax.plot(azimuths_smooth, powers_smooth, '-', label=str(freq))
+		ax.set_theta_zero_location('N')
+		ax.set_ylim(-70, 10)
+		ax.set_xticks(np.arange(0, 360, 10) * (np.pi / 180))
+		ax.set_yticks(np.arange(-70, 30, 10))
+		ax.set_rlabel_position(-np.pi / 2)
+		# ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+		#           ncol=len(FREQS), mode="expand", borderaxespad=0, prop={'size': 6})
+	plt.savefig(filename)
+	plt.close()
+
+def analysis(references, sample_duration=SAMPLE_DURATION):
+	azimuth_files = get_files(references)
+	azimuth_powers = extract_powers(azimuth_files, sample_duration)
+	db_azimuths_freqs = normalize(azimuth_powers)
+	plot_gain_pattern(
+		azimuth_powers,
+		db_azimuths_freqs, '../output/analysis-mean-sample-%s.png' % (sample_duration)
+	)
+
+if __name__ == "__main__":
+	references = {
+		"../2017-7-12/azimuth-2.csv": "../2017-7-12/azimuth-2/DR0000_%s.wav",
+		"../2017-7-12/azimuth-1.csv": "../2017-7-12/azimuth-1/DR0000_%s.wav",
+	}
+	for d in xrange(5,55,5):
+		analysis(references, 0.001 * d)
---
+++ analysis/segment.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, cheby2, medfilt
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+def bandpass_coefficients(lowcut, highcut, fs, order=10):
+	nyq = 0.5 * fs
+	low = lowcut / nyq
+	high = highcut / nyq
+	b, a = cheby2(order, 50, [low,high], btype='bandpass')
+	return b, a
+
+def bandpass(data, low, high, rate, order=None):
+	bpf = bandpass_coefficients(low, high, rate, order)
+	return lfilter(bpf[0], bpf[1], data)
+
+def band_powers(data, fs, freqs, pulse_duration,
+		sample_duration, bandwidth=100, debug=False):
+	pulse_length = int(fs * pulse_duration)
+	sample_length = int(fs * sample_duration)
+	tones = []
+	powers = {}
+	for freq in freqs:
+		avg_powers = []
+		for channel in xrange(data.shape[1]):
+			band = bandpass(data[:,channel], freq - bandwidth, freq + bandwidth, fs, 3)
+			tone_start = np.argmax(moving_average(band, pulse_length)) - pulse_length
+			tones.append(tone_start)
+			avg_powers.append(
+				(1.0 / pulse_length) * np.sum(
+					band[tone_start:tone_start+sample_length] ** 2
+				)
+			)
+		powers[freq] = np.mean(avg_powers)
+	if debug:
+		plt.plot(np.array(tones) / fs, freqs, 'bx')
+		plt.specgram(data, Fs=fs, NFFT=1024)
+		plt.show()
+	return powers
